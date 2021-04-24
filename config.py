aa51f5f3d05658394d1f072072c899332b830477
Bu11erkek5 <buetttim@gmail.com>
2021-04-24 12:46:04 +0200

    Initial Commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# Kafka configuration
+conf = {
+    "bootstrap.servers": "pkc-w7d6j.germanywestcentral.azure.confluent.cloud:9092",
+    "security.protocol": "SASL_SSL",
+    "sasl.mechanisms": "PLAIN",
+    "sasl.username": "KFH2UWPVQIFA5CUB",
+    "sasl.password": "AY879rCiyJeZx5MJz5gEywn1+JJv0W7z2PqgFPkBhB/ozySgcv6W8hgoa7vEuJYS",
+    "group.id": "simple_python_consumer",
+    "auto.offset.reset": "earliest"
+}
